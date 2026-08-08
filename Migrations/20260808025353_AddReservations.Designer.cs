2132fcb9f46cdf8855b3f642d0c8def5fbe8a717
Sri Sai Chandra Sekhar Kattunga <skattunga@govst.edu>
2026-08-07 21:53:53 -0500

    Add Reservations migration

--- Migrations/20260808025353_AddReservations.Designer.cs
+++ Migrations/20260808025353_AddReservations.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Picklr.Models;
 
@@ -10,9 +11,11 @@ using Picklr.Models;
 namespace Picklr.Migrations
 {
     [DbContext(typeof(PicklrContext))]
-    partial class PicklrContextModelSnapshot : ModelSnapshot
+    [Migration("20260808025353_AddReservations")]
+    partial class AddReservations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.0");
---
+++ Migrations/20260808025353_AddReservations.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Picklr.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReservations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Clubs",
+                type: "TEXT",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Clubs",
+                type: "TEXT",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 500);
+        }
+    }
+}
